b56879be4aa76fb371dca3183ef93ad34b148b62
Soylinaa <linabrinez.mr@academia.umb.edu.co>
2025-05-26 13:18:42 -0500

    Update settings.py

--- puppi_tinder/settings.py
+++ puppi_tinder/settings.py
@@ -18,6 +18,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = [".railway.app", "localhost", "127.0.0.1"]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://musik-production.up.railway.app'
+]
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
