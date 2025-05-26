943bc01fe328f6f1e77bd06604db24fa2fd5d57b
Soylinaa <linabrinez.mr@academia.umb.edu.co>
2025-05-26 13:09:53 -0500

    Delete LINA PUPPI- TINDER/puppi-tinder-django directory

--- LINA PUPPI- TINDER/puppi-tinder-django/apps/adoption/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('dogs/', views.DogListView.as_view(), name='dog-list'),
-    path('dogs/<int:pk>/', views.DogDetailView.as_view(), name='dog-detail'),
-    path('apply/', views.submit_adoption_application, name='submit-application'),
-    path('my-applications/', views.my_applications, name='my-applications'),
-]
